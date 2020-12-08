800d961c5908b737662c8f987beb80e10c8c0d7e
Esteban <esteban.porqueres@free.fr>
2020-12-08 13:22:52 +0100

    update info patient
    manque juste a recup l'id via session

--- patient_home.php
+++ patient_home.php
@@ -1,3 +1,13 @@
+<?php
+    require_once("db_object_patient.php");
+    $dao= new DAO();
+    if ($dao->getERROR()) {
+        print "erreur: ".$dao->getError();
+    }
+    $item=$dao->getNomPrenom("mutuelle.nom as mutuelle,praticien.nom as praticien,patient.nom,patient.prenom,patient.sexe,patient.nom_naissance,patient.date_naissance,patient.telephone_portable,patient.telephone_fixe,patient.email,patient.adresse1,patient.adresse2,patient.code_postal,patient.ville,patient.pays,patient.numero_securite_sociale",1);   
+    // print_r($item);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,7 +19,7 @@
 </head>
 <body>
 
-<div class="container"><h3>M/Mme. NOM Prenom</h3></div>
+<div class="container"><h3><?= $item[0]["sexe"]?>. <?= $item[0]["nom"]?> <?= $item[0]["prenom"]?></h3></div>
 <div id="exTab1" class="container">	
     <ul class="nav nav-pills">
         <li class="active"><a  href="#1a" data-toggle="tab">Vos informations personnelle</a>
@@ -21,52 +31,54 @@
     </ul>
     <div class="tab-content clearfix">
         <div class="tab-pane active" id="1a">
-            <h7>info perso</h7>
+            <h7>yo</h7>
             <div class="white_background_conteneur">
                 <div id="conteneur_type_information">
                     <br>
                     <br>
-                    <p>Sexe :</p>
-                    <p>Nom :</p>
-                    <p>Prénom :</p>
-                    <p>Nom de naissance :</p>
-                    <p>Date de naissance :</p>
-                    <p>Téléphone :</p>
-                    <p>Email:</p>
-                    <p>Adresse1 :</p>
-                    <p>Adresse2 :</p>
-                    <p>Code postal :</p>
-                    <p>Ville :</p>
-                    <p>Pays :</p>
+                    <br>Sexe <br>
+                    <br>Nom <br>
+                    <br>Prénom <br>
+                    <br>Nom de naissance <br>
+                    <br>Date de naissance <br>
+                    <br>Téléphone portable <br>
+                    <br>Téléphone fixe<br>
+                    <br>Email<br>
+                    <br>Adresse1 <br>
+                    <br>Adresse2 <br>
+                    <br>Code postal <br>
+                    <br>Ville <br>
+                    <br>Pays <br>
                     <br>
                     <hr>
                     <br>
-                    <p>Numéros de securité social :</p>
-                    <p>Praticien :</p>
-                    <p>Mutuelle :</p>
+                    <br>Numéros de securité social <br>
+                    <br>Praticien <br>
+                    <br>Mutuelle <br>
                     <br>
                 </div>
                 <div id="conteneur_information">
                     <br>
                     <br>
-                    <p>$Sexe :</p>
-                    <p>$Nom :</p>
-                    <p>$Prénom :</p>
-                    <p>$Nom de naissance :</p>
-                    <p>$Date de naissance :</p>
-                    <p>$Téléphone :</p>
-                    <p>$Email:</p>
-                    <p>$Adresse1 :</p>
-                    <p>$Adresse2 :</p>
-                    <p>$Code postal :</p>
-                    <p>$Ville :</p>
-                    <p>$Pays :</p>
+                    <br><?= $item[0]["sexe"]?><br>
+                    <br><?= $item[0]["nom"]?><br>
+                    <br><?= $item[0]["prenom"]?><br>
+                    <br><?= $item[0]["nom_naissance"]?><br>
+                    <br><?= date("d/m/Y", strtotime($item[0]["date_naissance"]));?><br>
+                    <br><?= $item[0]["telephone_portable"]?><br>
+                    <br><?= $item[0]["telephone_fixe"]?><br>
+                    <br><?= $item[0]["email"]?><br>
+                    <br><?= $item[0]["adresse1"]?><br>
+                    <br><?= $item[0]["adresse2"]?><br>
+                    <br><?= $item[0]["code_postal"]?><br>
+                    <br><?= $item[0]["ville"]?><br>
+                    <br><?= $item[0]["pays"]?><br>
                     <br>
                     <hr>
                     <br>
-                    <p>$Numéros de securité social :</p>
-                    <p>Praticien :</p>
-                    <p>Mutuelle :</p>
+                    <br><?= $item[0]["numero_securite_sociale"]?><br>
+                    <br><?= $item[0]["praticien"]?><br>
+                    <br><?= $item[0]["mutuelle"]?><br>
                     <br>
                 </div>
             </div>
@@ -74,10 +86,14 @@
 
         </div>
         <div class="tab-pane" id="2a">
-            <h7>We use the class nav-pills instead of nav-tabs which automatically creates a background color for the tab</h7>
+            <h7>ca va ?</h7>
+            <div class="white_background_conteneur">
+            </div>
         </div>
         <div class="tab-pane" id="3a">
-            <h7>We applied clearfix to the tab-content to rid of the gap between the tab and the content</h7>
+            <h7>grave</h7>
+            <div class="white_background_conteneur">
+            </div>
         </div>
     </div>
 </div>
