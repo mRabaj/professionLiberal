8cdd90c41499a99502ea639765d4c1e0cc1644fc
Esteban <esteban.porqueres@free.fr>
2020-12-08 08:27:07 +0100

    update patient_home php & css

--- patient_home.php
+++ patient_home.php
@@ -4,20 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="patient_home.css">
-    <script src="jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Home patient</title>
 </head>
 <body>
 
-<div class="container"><h1>home patient</h1></div>
+<div class="container"><h3>NOM Prenom</h3></div>
 <div id="exTab1" class="container">	
     <ul class="nav nav-pills">
-        <li class="active"><a  href="#1a" data-toggle="tab">info perso</a>
+        <li class="active"><a  href="#1a" data-toggle="tab">Vos informations personnelle</a>
         </li>
-        <li><a href="#2a" data-toggle="tab">prendre rdv</a>
+        <li><a href="#2a" data-toggle="tab">Prendre rendez-vous</a>
         </li>
-        <li><a href="#3a" data-toggle="tab">envoi doc</a>
+        <li><a href="#3a" data-toggle="tab">Envoi de document</a>
         </li>
     </ul>
     <div class="tab-content clearfix">
