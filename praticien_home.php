539b84ad77f2fc34b35e67d3d962617be158552c
Esteban <esteban.porqueres@free.fr>
2020-12-08 19:20:19 +0100

    css pour praticien_home

--- praticien_home.php
+++ praticien_home.php
@@ -3,9 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="praticien_home.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Home praticien</title>
 </head>
 <body>
-    
+
+    <div class="container"><h3>M/Mme nom prenom</h3></div>
+    <div id="exTab1" class="container">	
+        <ul class="nav nav-pills">
+            <li class="active"><a  href="#1a" data-toggle="tab">Liste passiant</a>
+            </li>
+            <li><a href="#2a" data-toggle="tab">Nouveau passiant</a>
+            </li>
+            <li><a href="#3a" data-toggle="tab">Emplois du temps</a>
+            </li>
+        </ul>
+        <div class="tab-content clearfix">
+            <div class="tab-pane active" id="1a">
+                <h7>yo</h7>
+                <div class="white_background_conteneur">
+                    test
+                </div>
+                test
+            </div>
+
+            <div class="tab-pane" id="2a">
+                <h7>ca va ?</h7>
+                <div class="white_background_conteneur">
+                </div>
+            </div>
+
+            <div class="tab-pane" id="3a">
+                <h7>grave</h7>
+                <div class="white_background_conteneur">
+                </div>
+                test
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 </body>
 </html>
